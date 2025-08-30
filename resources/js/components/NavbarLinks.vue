6e3044542cfe8c68f2d73aef21b765a1f37a2a16
jesusemo <gamateo2000@gmail.com>
2025-08-30 07:33:48 -0300

    good practices to separate links from navbar, added a svg icon and formated with tailwind the navbar

---
+++ resources/js/components/NavbarLinks.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex">
+      <div v-for="(page) in pages" class="flex justify-between px-2 items-center">
+        
+        <a :href='slugify(page)' class="md:text-sm text-xs font-bold">
+          {{ page }}
+        </a>
+      </div>
+    </div>
+
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { slugify } from '../composables/useSlugify';
+// evitar el uso de la "ñ"
+const pages = ref(["Inicio", "Sobre Mí", "Contacto", "Acerca de", "eñe"]);
+</script>
